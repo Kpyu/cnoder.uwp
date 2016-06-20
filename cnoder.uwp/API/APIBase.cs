9d0c6e9db268714648751cfcac1f9d61ced9fe83
Kp Yu <Kp Yu>
2016-06-21 00:13:39 +0800

    update

---
+++ cnoder.uwp/API/APIBase.cs
@@ -0,0 +1,27 @@
+﻿using cnoder.uwp.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnoder.uwp.API
+{
+    
+    abstract class  APIBase:HttpHelper
+    {
+        const string apiHost = "http://cnodejs.org/api/v1";
+        protected  String TOPICS = $"{apiHost}/topics";
+        protected  String TOPIC = $"{apiHost}/topic";
+        protected  String REPLY_2_TOPIC = $"{apiHost}/topic";
+        protected  String ADD_COLLECT = $"{apiHost}/topic_collect/collect";
+        protected  String DEL_COLLECT = $"{apiHost}/topic_collect/de_collect";
+        protected  String USER_COLLECT = $"{apiHost}/topic_collect";
+        protected  String AGREE_REPLY = $"{apiHost}/reply";
+        protected  String AUTHORIZE = $"{apiHost}/accesstoken";
+        protected  String USER = $"{apiHost}/user";
+        protected  String MESSAGES = $"{apiHost}/messages";
+        protected  String MESSAGE_COUNT = $"{apiHost}/message/count";
+        protected  String MARK_ALL_MESSAGE = $"{apiHost}/message/mark_all";
+    }
+}
---
+++ cnoder.uwp/Helper/HttpHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cnoder.uwp.Helper
+{
+    public class HttpHelper
+    {
+        public static HttpHelper Factory
+        {
+            get
+            {
+                return new HttpHelper();
+            }
+        }
+        public HttpRequestMessage CreateRequest(HttpMethod method, string uri, params KeyValuePair<string, string>[] keyValueCollection)
+        {
+            return CreateRequest(method, new Uri(uri), keyValueCollection);
+        }
+
+        private HttpRequestMessage CreateRequest(HttpMethod method, Uri uri, KeyValuePair<string, string>[] keyValueCollection)
+        {
+            var request = new HttpRequestMessage(method, uri);
+            request.Content = new FormUrlEncodedContent(keyValueCollection);
+            return request;
+        }
+    }
+}
